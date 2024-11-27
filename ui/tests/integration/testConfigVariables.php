2b3dbfb48be10bd0dd7fdc8baeda9f864e5efda8
Mihails Prihodko <mihails.prihodko@zabbix.com>
2024-11-27 18:24:37 +0200

    .......... [ZBXNEXT-9533] fixed typo in testConfigVariables test

--- ui/tests/integration/testConfigVariables.php
+++ ui/tests/integration/testConfigVariables.php
@@ -22,7 +22,7 @@ require_once dirname(__FILE__).'/../include/CIntegrationTest.php';
  * @onAfter cleanupTestEnv
  *
  */
-class testConfigTestOption extends CIntegrationTest {
+class testConfigVariables extends CIntegrationTest {
 	const SERVER_NAME = 'server';
 	const PROXY_NAME = 'proxy';
 	const STATS_ITEM_NAME = 'stats item';
