7eadfbfda39dbbe10e2525cbf90ae5d14cda5ef2
Iryna Sharha <iryna.sharha@zabbix.com>
2025-04-03 16:39:43 +0200

    ..F....... [ZBX-26120] removed unused code

--- ui/app/views/js/trigger.massupdate.js.php
+++ ui/app/views/js/trigger.massupdate.js.php
@@ -100,16 +100,5 @@
 		massupdate_overlay.$dialogue[0].addEventListener('dialogue.close', () => {
 			ZABBIX.EventHub.unsubscribeAll(subscriptions);
 		});
-
-		subscriptions.push(
-			ZABBIX.EventHub.subscribe({
-				require: {
-					context: CPopupManager.EVENT_CONTEXT,
-					event: CPopupManagerEvent.EVENT_END_SCRIPTING,
-					action: massupdate_overlay.dialogueid
-				},
-				callback: () => ZABBIX.EventHub.unsubscribeAll(subscriptions)
-			})
-		);
 	})();
 </script>
