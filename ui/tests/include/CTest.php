d4b73a9a1fad9a551a3ec040fa3ac712d383ae0b
Janis Nulle <janis.nulle@zabbix.com>
2025-06-16 11:14:36 +0300

    .......... [DEV-3126] added new Selenium tests for TOTP functionality
    * commit 'ddf2c0f700448ea5e3f0239fa5362cb190dc75fa': (57 commits)
      .......... [DEV-3126] optimized code after review
      .......... [DEV-3126] fixed indents according to previous approach
      .......... [DEV-3126] updated to __DIR__
      .......... [DEV-3126] fixedwhitespace
      .......... [DEV-3126] fixed after branch update
      .......... [DEV-3126] fix and refactor user login
      .......... [DEV-3126] referenced the correct status
      .......... [DEV-3126] move common onBefore code to parent class
      .......... [DEV-3126] PhpDoc fix
      .......... [DEV-3126] removed redundant backup table
      .......... [DEV-3126] broke comment in two line
      .......... [DEV-3126] fixed comment
      .......... [DEV-3126] rename digits to code length for more clarity
      .......... [DEV-3126] fixed method formatting
      .......... [DEV-3126] added whitespace
      .......... [DEV-3126] formatted phpdoc
      .......... [DEV-3126] fixed annotation whitespace
      .......... [DEV-3126] simplified Sign in button check
      .......... [DEV-3126] added check for empty value
      .......... [DEV-3126] small formatting
      ...
    
    (cherry picked from commit 160ee5d5a228fbc5cc23e5c1455f9e2482ad46d1)

--- ui/tests/include/CTest.php
+++ ui/tests/include/CTest.php
@@ -26,6 +26,7 @@ require_once __DIR__.'/helpers/CDataHelper.php';
 require_once __DIR__.'/helpers/CExceptionHelper.php';
 require_once __DIR__.'/helpers/CTestArrayHelper.php';
 require_once __DIR__.'/helpers/CDateTimeHelper.php';
+require_once __DIR__.'/helpers/CMfaTotpHelper.php';
 
 define('USER_ACTION_ADD', 'add');
 define('USER_ACTION_UPDATE', 'update');
